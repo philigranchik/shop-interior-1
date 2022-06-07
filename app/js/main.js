12246e2a3e59068e9f0d4b27a7df3a60986a69b1
Mikhail <poslak98@gmail.com>
2022-06-08 01:24:07 +0300

    1st commit

---
+++ app/js/main.js
@@ -0,0 +1,23 @@
+$(function () {
+    $('.header__btn').on('click', function() {
+        $('.rightside-menu').removeClass('rightside-menu--close')
+    });
+    $('.rightside-menu__close').on('click', function() {
+        $('.rightside-menu').addClass('rightside-menu--close')
+    });
+
+    $('.top__slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true
+    });
+
+    var mixer = mixitup('.gallery__inner', {
+        load: {
+            filter: '.living'
+        }
+    });
+
+
+})
