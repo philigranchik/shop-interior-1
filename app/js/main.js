8524372aa564845b0c13ed0182de5bca808088fb
Mikhail <poslak98@gmail.com>
2022-06-12 15:07:36 +0300

    ver2

--- app/js/main.js
+++ app/js/main.js
@@ -13,6 +13,14 @@ $(function () {
         autoplay: true
     });
 
+    $('.contact-slider').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 10,
+        slidesToScroll: 10
+    });
+    
+
     var mixer = mixitup('.gallery__inner', {
         load: {
             filter: '.living'
